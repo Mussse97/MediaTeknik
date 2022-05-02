b9e97a58d0a06317580489c883116d7a07b5bcca
Jacob Törnvall <Jacob.123@hotmail.se>
2022-05-02 05:18:20 -0700

    uwu

---
+++ js/script.js
@@ -0,0 +1,102 @@
+var choiceElem; // Parent till de vi bryr oss om
+const chosenAct = [
+	{altA:"Lugn", descA:"Vi vill ha en lugn aktivitet.", altB:"Aktiv", descB:"En aktivitet där man rör på sig passar oss."},
+	{altA:"Inomhus", descA:"Det är viktigt att aktiviteten är inomhus.", altB:"Utomhus", descB:"Vi skulle vilja ha en utomhus aktivitet."},
+	{altA:"Billigt", descA:"Aktiviteter under 500.", altB:"Dyrt", descB:"Aktiviteter övre 500.", altC:"Gratis", descC:"Gratis är gott!"},
+	{altA:"Småland", descA:"Visa bara aktiviterer i Småland.", altB:"Öland", descB:"Visa bara aktiviteter i Öland.", altC:"Båda", descC:"Visa aktiviteter i både Småland och Öland."}
+];
+const chosenRes = [
+	{altA:"Fint", descA:"Bara fina restauranger.", altB:"Snabbmat", descB:"Typ McDonalds HAHA", altC:"Exotiskt", descC:"Exotiska restauranger"},
+	{altA:"Uteservering", descA:"Det måste finnas uteservering!", altB:"Inomhus", descB:"Vi vill sitta inne."},
+	{altA:"Billigt", descA:"Restauranger som går under en 500 lapp.", altB:"Dyrt", descB:"Kostar över 500."},
+	{altA:"Småland", descA:"Visa bara restauranger i Småland.", altB:"Öland", descB:"Visa bara restauranger i Öland.", altC:"Båda", descC:"Visa restauranger i både Småland och Öland."}
+];
+		// Förslag: Buffe, vegetarisk, alkohol, 
+const choices = []; // Vilket alternativ som är valt
+var fraga = 0; // Vilken fråga är vi på?
+var progressElem; // Bollarna
+var chosenThing; // Vad är valt, aktivitet eller restaurang?
+
+// Initiera skit
+function init() {
+    choiceElem = document.querySelectorAll("#choice div");
+	progressElem = document.querySelectorAll("#progress div");
+
+	for (let i = 0; i < progressElem.length; i++) {
+		progressElem[i].addEventListener("click",rewind);
+		progressElem[i].setAttribute("data-ix",i);
+		progressElem[i].style.cursor = "not-allowed"; // Orkar inte kolla upp vad default cursorn heter...
+	}
+
+	for (let i = 0; i < choiceElem.length; i++) {
+		choiceElem[i].addEventListener("click",tuffing);
+		choiceElem[i].setAttribute("data-ix",i);
+	}
+
+}
+
+window.addEventListener("load",init);
+
+// Typ allt
+function stuff() { 
+	if (fraga == 5) { // ändra den här om du lägger till flera frågor
+		window.location.href += "valt.html?val=" + choices.toString(); 
+		return;
+	}
+	
+	for (let i = 0; i < progressElem.length; i++) { // Fixar bollarna
+		if (i < fraga) {
+			progressElem[i].style.cursor = "pointer";
+			progressElem[i].style.backgroundColor = "palevioletred";
+		}
+		else {
+			progressElem[i].style.cursor = "not-allowed";
+			progressElem[i].style.backgroundColor = "black";
+		}
+	}
+
+	if (fraga == 0) { // Ifall man börjar om helt
+		choiceElem[0].innerHTML = "<h3> Aktivitet </h3>";
+		choiceElem[1].innerHTML = "<h3> Restaurang </h3>";
+		choiceElem[2].innerHTML = "";
+		choiceElem[2].style.opacity = "0%";
+		return;
+	}
+
+	if (fraga == 1) { // Väljer rätt array 
+		if (choices[0] == 0) chosenThing = chosenAct;
+		else chosenThing = chosenRes;
+	}
+
+	let fr = fraga-1; // :(
+
+	choiceElem[2].style.opacity = "0%";
+
+	choiceElem[0].innerHTML = "<h3>" + chosenThing[fr].altA +"</h3>" + "<p>" + chosenThing[fr].descA +"</p>";
+	choiceElem[1].innerHTML = "<h3>" + chosenThing[fr].altB +"</h3>" + "<p>" + chosenThing[fr].descB +"</p>";
+
+	if (chosenThing[fr].altC != undefined) { // Ifall alternativ C finns
+		choiceElem[2].innerHTML = "<h3>" + chosenThing[fr].altC +"</h3>" + "<p>" + chosenThing[fr].descC +"</p>";
+		choiceElem[2].style.opacity = "100%";
+	}
+}
+
+// Bollarna
+function rewind() {
+	if (fraga >= this.getAttribute("data-ix")) { // Är det OK att klicka på den bollen?
+		let reduc = fraga-this.getAttribute("data-ix"); // Hur långt ska jag gå tillbaka? :<
+		for (let i = 0; i < reduc; i++) { // Ta bort val om man går tillbaka
+			choices.splice(-1,1);
+		}
+		fraga = this.getAttribute("data-ix"); // Ändrar skiten
+		stuff();
+	}
+}
+
+// Spara valen o nästa fråga
+function tuffing() {
+	fraga++;
+	let k = this.getAttribute("data-ix");
+	choices.push(k);
+	stuff();
+}
---
+++ js/valt.js
@@ -0,0 +1,79 @@
+var stepElem;
+var fixedCode;
+var api_key = "FqZF2ASN";
+const chosenAct = [
+    {urlA:"activity"},
+    {urlA:"xd", urlB:"x3"},
+    {urlA:"xd", urlB:"x3"},
+    {urlA:"xd", urlB:"x3", urlC:"uwu"},
+    {urlA:"", urlB:"", urlC:""},
+];
+const chosenFood = [
+    {urlB:"food"},
+    {urlA:"&types=FINE_DINING", urlB:"&types=FAST", urlC:"&types=ETHNIC"},
+    {urlA:"&outdoor_seating=Y", urlB:"&indoor_seating=Y"},
+    {urlA:"&max_avg_dinner_pricing=500", urlB:"&min_avg_dinner_pricing=500"},
+    {urlA:"", urlB:"", urlC:""}, //Provinces finns bara i establshment taggen...
+];
+
+//{altA:"Fint", descA:"Bara fina restauranger.", altB:"Snabbmat", descB:"Typ McDonalds HAHA", altC:"Exotiskt", descC:"Exotiska restauranger"},
+//{altA:"Uteservering", descA:"Det måste finnas uteservering!", altB:"Inomhus", descB:"Vi vill sitta inne."},
+//{altA:"Billigt", descA:"Restauranger som går under en 500 lapp.", altB:"Dyrt", descB:"Kostar över 500."},
+//{altA:"Småland", descA:"Visa bara aktiviterer i Småland.", altB:"Öland", descB:"Visa bara aktiviteter i Öland.", altC:"Båda", descC:"Visa aktiviteter i både Småland och Öland."}
+
+function init() {
+    stepElem = document.getElementById("stepElement");
+    
+    fixedCode = fixCode(window.location.search);
+
+    if (fixCode[0] == 0) getController(chosenAct);
+    else getController(chosenFood);
+}
+
+window.addEventListener("load",init);
+
+function fixCode(code) {
+    code = code.split('=');
+    code = code[1];
+    code = code.split(',');
+    for (let i = 0; i < code.length; i++) code[i] = parseInt(code[i]);
+    return code;
+}
+
+function getController(uwu) {
+    let controller = [];
+   
+    for (let i = 0; i < uwu.length; i++) {
+        if (fixedCode[i] == 0) controller.push(uwu[i].urlA);
+        else if (fixedCode[i] == 1) controller.push(uwu[i].urlB);
+        else controller.push(uwu[i].urlC);
+    }
+
+    applyController(controller);
+}
+
+function applyController(xd) {
+    let request = new XMLHttpRequest(); 
+
+    request.open("GET","https://smapi.lnu.se/api/?api_key=" + api_key + "&controller=" + xd[0] + "&method=getall&per_page=3&order_by=rating" + xd[1] + xd[2] + xd[3],true);
+    request.send(null); 
+    request.onreadystatechange = function () {
+        if (request.readyState == 4)
+            if (request.status == 200) listAlts(request.responseText);
+            else stepElem.innerHTML = "Nåt gick fel";
+    };
+}
+
+function listAlts(owo) {
+    owo = JSON.parse(owo);
+    owo = owo.payload;
+    console.log(owo);
+    if (owo.length == 0) {
+        stepElem.innerHTML = "Finns inga resultat :<"
+        return;
+    }
+    console.log(owo);
+    for (let i = 0; i < owo.length; i++) {
+        stepElem.innerHTML += "<h3>" + owo[i].name + "</h3>"
+    }
+}
