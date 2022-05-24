d93df75d73bfd5288bf0e88e203c7a8cf88b78d5
Jacob Törnvall <Jacob.123@hotmail.se>
2022-05-25 01:19:44 +0200

    GIGA UWU SMOLL FIX (COZ IM RETARDED)

--- js/valt.js
+++ js/valt.js
@@ -264,8 +264,7 @@ function initMap(wow) {
 }
 
 function getLocation(wow,map) {
-    alert("gay");
-    if (navigator.geolocation != undefined) {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (p) {
             let LatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
 
